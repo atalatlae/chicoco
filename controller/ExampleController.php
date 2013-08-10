292015006cc20a6f8105d1dbef37f8694ea2a408
Boris <atalatlae@gmail.com>
2013-08-10 18:26:59 -0400

    Added a ExampleController
    - Added a basic form processing example

---
+++ controller/ExampleController.php
@@ -0,0 +1,29 @@
+<?php
+
+class ExampleController extends Controller
+{
+	public function init() {
+	}
+
+	public function IndexAction() {
+		$this->render();
+	}
+
+	public function FormAction() {
+		
+		$send = $this->getPostVar('send', 'string');
+
+		if (isset($send) and $send != '') {
+			$name = $this->getPostVar('name', 'string');
+
+			if (isset($name) and $name != '') {
+				$formResult = 'Form OK, you text is: '.$name;
+			}
+			else {
+				$formResult = 'Please, write some text';
+			}
+			$this->setViewVar('formResult', $formResult);
+		}
+		$this->render();
+	}
+}
