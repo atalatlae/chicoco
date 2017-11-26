78341ab8619e82d849c6b3cce8c548703f2f0ee6
Boris <atalatlae@gmail.com>
2017-11-26 00:59:47 -0300

    [apply_phpcs] replace tabs by spaces into core/Init.php

--- core/Init.php
+++ core/Init.php
@@ -4,25 +4,25 @@ namespace Chicoco;
 
 class Init
 {
-	function __construct() {
-		spl_autoload_register(array($this, "autoLoadClass"));
-	}
+    function __construct() {
+        spl_autoload_register(array($this, "autoLoadClass"));
+    }
 
-	public function autoLoadClass($className)
-	{
-		$className = str_replace("Chicoco\\", '', $className);
+    public function autoLoadClass($className)
+    {
+        $className = str_replace("Chicoco\\", '', $className);
 
-		if (is_file("core/".$className.".php")) {
-			include_once("core/".$className.".php");
-		}
-		else if (is_file("controller/".$className.".php")) {
-			include_once("controller/".$className.".php");
-		}
-		elseif (is_file("model/".$className.".php")) {
-			include_once("model/".$className.".php");
-		}
-		else {
-			throw new \Exception("Unable to load class $className.");
-		}
-	}
+        if (is_file("core/".$className.".php")) {
+            include_once("core/".$className.".php");
+        }
+        else if (is_file("controller/".$className.".php")) {
+            include_once("controller/".$className.".php");
+        }
+        elseif (is_file("model/".$className.".php")) {
+            include_once("model/".$className.".php");
+        }
+        else {
+            throw new \Exception("Unable to load class $className.");
+        }
+    }
 }
