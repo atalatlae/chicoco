4bcc6ad75d122afa6509d6816859a40ff2b46831
Boris <atalatlae@gmail.com>
2017-11-26 01:06:43 -0300

    [apply_phpcs] replace tabs by spaces into core/LogDb.php

--- core/LogDb.php
+++ core/LogDb.php
@@ -4,45 +4,45 @@ namespace Chicoco;
 
 class LogDb extends Log
 {
-	private $_db;
-	private $_levels = array(
-		"6" => "LOG_INFO",
-		"4" => "LOG_WARNING",
-		"3" => "LOG_ERR"
-	);
+    private $_db;
+    private $_levels = array(
+        "6" => "LOG_INFO",
+        "4" => "LOG_WARNING",
+        "3" => "LOG_ERR"
+    );
 
-	public function __construct() {
-		$this->_db = new Dao();
-	}
+    public function __construct() {
+        $this->_db = new Dao();
+    }
 
-	protected function _write($message = '', $controller = '', $action = '') {
-		$level = $this->_logLevel;
+    protected function _write($message = '', $controller = '', $action = '') {
+        $level = $this->_logLevel;
 
-		try {
-			$l = $this->_levels["$level"];
+        try {
+            $l = $this->_levels["$level"];
 
-			$this->_db->setSql('INSERT INTO logs '
-			.'(timestamp, level, message, controller, action) VALUES '
-			.'(NOW(), :level, :message, :controller, :action)');
-			$this->_db->clearParams();
-			$this->_db->addParam(':level',      $l,         \PDO::PARAM_STR);
-			$this->_db->addParam(':message',    $message,   \PDO::PARAM_STR);
-			$this->_db->addParam(':controller', $controller,\PDO::PARAM_STR);
-			$this->_db->addParam(':action',     $action,    \PDO::PARAM_STR);
+            $this->_db->setSql('INSERT INTO logs '
+            .'(timestamp, level, message, controller, action) VALUES '
+            .'(NOW(), :level, :message, :controller, :action)');
+            $this->_db->clearParams();
+            $this->_db->addParam(':level',      $l,         \PDO::PARAM_STR);
+            $this->_db->addParam(':message',    $message,   \PDO::PARAM_STR);
+            $this->_db->addParam(':controller', $controller,\PDO::PARAM_STR);
+            $this->_db->addParam(':action',     $action,    \PDO::PARAM_STR);
 
-			$this->_db->doInsert();
-			$query = $this->_db->getResult();
+            $this->_db->doInsert();
+            $query = $this->_db->getResult();
 
-			if ($query === false) {
-				$error = $this->_db->getMsgResult();
-				throw new \Exception('Log: '.var_export($error, true));
-			}
+            if ($query === false) {
+                $error = $this->_db->getMsgResult();
+                throw new \Exception('Log: '.var_export($error, true));
+            }
 
-			return true;
-		}
-		catch (\Exception $e) {
-			$this->_msgResult = $e->getMessage();
-			return false;
-		}
-	}
+            return true;
+        }
+        catch (\Exception $e) {
+            $this->_msgResult = $e->getMessage();
+            return false;
+        }
+    }
 }
