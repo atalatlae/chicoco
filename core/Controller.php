6c35e4fea7eda3345426d54524832d44acb3cbfd
Boris <atalatlae@gmail.com>
2017-11-26 00:37:02 -0300

    [apply_phpcs] apply PSR2 into core/Controller.php

--- core/Controller.php
+++ core/Controller.php
@@ -4,75 +4,80 @@ namespace Chicoco;
 
 class Controller
 {
-    protected $_controller;
-    protected $_action;
-    protected $_defaultLayout = 'default';
-    protected $_pathParams = array();
-    protected $_scripts;
-    protected $_log;
+    protected $controller;
+    protected $action;
+    protected $defaultLayout = 'default';
+    protected $pathParams = array();
+    protected $scripts;
+    protected $log;
 
-    private   $_data = array();
+    private $data = array();
 
-    public function __construct() {
-        $this->_log = new Log();
+    public function __construct()
+    {
+        $this->log = new Log();
     }
 
-    public function __call($name, $params) {
+    public function __call($name, $params)
+    {
         return false;
     }
 
-    public function init() {
+    public function init()
+    {
     }
 
-    public function setController($controller) {
-        $this->_controller = $controller;
+    public function setController($controller)
+    {
+        $this->controller = $controller;
         return $this;
     }
 
-    public function setAction($action) {
-        $this->_action = $action;
+    public function setAction($action)
+    {
+        $this->action = $action;
         return $this;
     }
 
-    public function setPathParams(array $params) {
-        $this->_pathParams = $params;
+    public function setPathParams(array $params)
+    {
+        $this->pathParams = $params;
         return $this;
     }
 
-    public function getPathParams($name = '', $type = 'string') {
+    public function getPathParams($name = '', $type = 'string')
+    {
         if ($name == '') {
-            return $this->_pathParams;
-        }
-        else {
-            if (array_key_exists($name, $this->_pathParams)) {
-                return $this->sanitizeVar($this->_pathParams[$name], $type);
-            }
-            else {
-                return NULL;
+            return $this->pathParams;
+        } else {
+            if (array_key_exists($name, $this->pathParams)) {
+                return $this->sanitizeVar($this->pathParams[$name], $type);
+            } else {
+                return null;
             }
         }
     }
 
-    public function render($layout = '', $fileName = '') {
+    public function render($layout = '', $fileName = '')
+    {
 
         if ($layout != '' && is_file('layout/'.$layout.'.phtml')) {
-            $this->_defaultLayout = $layout;
+            $this->defaultLayout = $layout;
         }
 
-        $viewDir = preg_replace('/(.*)Controller$/', '${1}', $this->_controller);
+        $viewDir = preg_replace('/(.*)Controller$/', '${1}', $this->controller);
 
         if ($fileName != '') {
             if (!is_file('view/'.$viewDir.'/'.$fileName.'.phtml')) {
-                $fileName = $this->_action;
+                $fileName = $this->action;
             }
-        }
-        else {
-            $fileName = $this->_action;
+        } else {
+            $fileName = $this->action;
         }
 
         // Put the variables visible to the included file
-        if (is_array($this->_data)) {
-            foreach($this->_data as $k => $v) {
+        if (is_array($this->data)) {
+            foreach ($this->data as $k => $v) {
                 ${$k}  = $v;
             }
         }
@@ -82,59 +87,70 @@ class Controller
         $content = ob_get_contents();
         ob_end_clean();
 
-        include('layout/'.$this->_defaultLayout.'.phtml');
+        include('layout/'.$this->defaultLayout.'.phtml');
         exit();
     }
 
-    public function redirect($path = '') {
+    public function redirect($path = '')
+    {
         if ($path != '') {
             header('Location: '.$path);
             exit();
         }
     }
 
-    public function setViewVar($name, $value) {
+    public function setViewVar($name, $value)
+    {
         if ($name != '') {
-            $this->_data[$name] = $value;
+            $this->data[$name] = $value;
         }
     }
 
-    public function setViewScript($script) {
+    public function setViewScript($script)
+    {
         if ($script != '') {
-            $this->_scripts[] = $script;
-            $this->setViewVar('viewScripts', $this->_scripts);
+            $this->scripts[] = $script;
+            $this->setViewVar('viewScripts', $this->scripts);
         }
     }
 
-    public function logInfo($message = '') {
-        return $this->_log->info($message, $this->_controller, $this->_action);
+    public function logInfo($message = '')
+    {
+        return $this->log->info($message, $this->controller, $this->action);
     }
 
-    public function logWarning($message = '') {
-        return $this->_log->warning($message, $this->_controller, $this->_action);
+    public function logWarning($message = '')
+    {
+        return $this->log->warning($message, $this->controller, $this->action);
     }
 
-    public function logError($message = '') {
-        return $this->_log->error($message, $this->_controller, $this->_action);
+    public function logError($message = '')
+    {
+        return $this->log->error($message, $this->controller, $this->action);
     }
 
-    public function getUrlVar($name = '', $type = '') {
-        return $this->_getVar($name, $type, 'get');
+    public function getUrlVar($name = '', $type = '')
+    {
+        return $this->getVar($name, $type, 'get');
     }
 
-    public function getPostVar($name = '', $type = '') {
-        return $this->_getVar($name, $type, 'post');
+    public function getPostVar($name = '', $type = '')
+    {
+        return $this->getVar($name, $type, 'post');
     }
 
-    public function getRequestVar($name = '', $type = '') {
-        return $this->_getVar($name, $type, 'request');
+    public function getRequestVar($name = '', $type = '')
+    {
+        return $this->getVar($name, $type, 'request');
     }
 
-    public function getFileVar($name = '') {
-        return $this->_getVar($name, '', 'file');
+    public function getFileVar($name = '')
+    {
+        return $this->getVar($name, '', 'file');
     }
 
-    private function _getVar($name = '', $type = '', $from = 'get') {
+    private function getVar($name = '', $type = '', $from = 'get')
+    {
         switch ($from) {
             case 'get':
                 if (isset($_GET[$name])) {
@@ -175,13 +191,13 @@ class Controller
 
         if (isset($filters[$type])) {
             return filter_var($var, $filters[$type]);
-        }
-        else {
+        } else {
             return $var;
         }
     }
 
-    protected function dumpVar($var) {
+    protected function dumpVar($var)
+    {
         $s = '<pre>'
         .var_export($var, true)
         .'</pre>';
