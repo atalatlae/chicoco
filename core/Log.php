c4896038ef651967eb7484f48f1b92d5e98bbc4a
Boris <atalatlae@gmail.com>
2017-11-26 01:02:45 -0300

    [apply_phpcs] replace tabs by spaces into core/Log.php

--- core/Log.php
+++ core/Log.php
@@ -4,42 +4,42 @@ namespace Chicoco;
 
 class Log
 {
-	protected $_logLevel = LOG_INFO;
-	protected $_msgResult;
+    protected $_logLevel = LOG_INFO;
+    protected $_msgResult;
 
-	public function info($message = '', $controller = '', $action = '') {
-		if ($message != '') {
-			$this->_logLevel = LOG_INFO;
-			return $this->_write($message, $controller, $action);
-		}
-	}
+    public function info($message = '', $controller = '', $action = '') {
+        if ($message != '') {
+            $this->_logLevel = LOG_INFO;
+            return $this->_write($message, $controller, $action);
+        }
+    }
 
-	public function warning($message = '', $controller = '', $action = '') {
-		if ($message != '') {
-			$this->_logLevel = LOG_WARNING;
-			return $this->_write($message, $controller, $action);
-		}
-	}
+    public function warning($message = '', $controller = '', $action = '') {
+        if ($message != '') {
+            $this->_logLevel = LOG_WARNING;
+            return $this->_write($message, $controller, $action);
+        }
+    }
 
-	public function error($message = '', $controller = '', $action = '') {
-		if ($message != '') {
-			$this->_logLevel = LOG_ERR;
-			return $this->_write($message, $controller, $action);
-		}
-	}
+    public function error($message = '', $controller = '', $action = '') {
+        if ($message != '') {
+            $this->_logLevel = LOG_ERR;
+            return $this->_write($message, $controller, $action);
+        }
+    }
 
-	public function getMsgResult() {
-		return $this->_msgResult;
-	}
+    public function getMsgResult() {
+        return $this->_msgResult;
+    }
 
-	protected function _write($message = '', $controller = '', $action = '') {
-		try {
-			syslog($this->_logLevel, "$controller/$action: $message");
-			return true;
-		}
-		catch (\Exception $e) {
-			$this->_msgResult = $e->getMessage();
-			return false;
-		}
-	}
+    protected function _write($message = '', $controller = '', $action = '') {
+        try {
+            syslog($this->_logLevel, "$controller/$action: $message");
+            return true;
+        }
+        catch (\Exception $e) {
+            $this->_msgResult = $e->getMessage();
+            return false;
+        }
+    }
 }
