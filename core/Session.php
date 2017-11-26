973c40b5565c2c70db4cc55aa5d427e1374e9074
Boris <atalatlae@gmail.com>
2017-11-26 01:20:04 -0300

    [apply_phpcs] replace tabs by spaces into core/Session.php

--- core/Session.php
+++ core/Session.php
@@ -4,29 +4,29 @@ namespace Chicoco;
 
 class Session extends Singleton
 {
-	public function __construct() {
-		session_start();
-	}
+    public function __construct() {
+        session_start();
+    }
 
-	public function setVar($name, $value) {
-		if ($name != "") {
-			$_SESSION[$name] = $value;
-		}
-	}
+    public function setVar($name, $value) {
+        if ($name != "") {
+            $_SESSION[$name] = $value;
+        }
+    }
 
-	public function getVar($name) {
-		if ($name != "" && isset($_SESSION[$name]) ) {
-			return $_SESSION[$name];
-		}
-		return null;
-	}
+    public function getVar($name) {
+        if ($name != "" && isset($_SESSION[$name]) ) {
+            return $_SESSION[$name];
+        }
+        return null;
+    }
 
-	public function getSessionId() {
-		return session_id();
-	}
+    public function getSessionId() {
+        return session_id();
+    }
 
-	public function destroy() {
-		unset($_SESSION);
-		session_destroy();
-	}
+    public function destroy() {
+        unset($_SESSION);
+        session_destroy();
+    }
 }
