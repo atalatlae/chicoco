98a7d0d4fccfd00b4a3badd6bceedfec05d6b23d
Boris <atalatlae@gmail.com>
2017-11-26 01:10:04 -0300

    [apply_phpcs] apply PSR2 into core/LogDb.php

--- core/LogDb.php
+++ core/LogDb.php
@@ -4,43 +4,44 @@ namespace Chicoco;
 
 class LogDb extends Log
 {
-    private $_db;
-    private $_levels = array(
+    private $db;
+    private $levels = array(
         "6" => "LOG_INFO",
         "4" => "LOG_WARNING",
         "3" => "LOG_ERR"
     );
 
-    public function __construct() {
-        $this->_db = new Dao();
+    public function __construct()
+    {
+        $this->db = new Dao();
     }
 
-    protected function _write($message = '', $controller = '', $action = '') {
+    protected function write($message = '', $controller = '', $action = '')
+    {
         $level = $this->_logLevel;
 
         try {
-            $l = $this->_levels["$level"];
+            $l = $this->levels["$level"];
 
-            $this->_db->setSql('INSERT INTO logs '
+            $this->db->setSql('INSERT INTO logs '
             .'(timestamp, level, message, controller, action) VALUES '
             .'(NOW(), :level, :message, :controller, :action)');
-            $this->_db->clearParams();
-            $this->_db->addParam(':level',      $l,         \PDO::PARAM_STR);
-            $this->_db->addParam(':message',    $message,   \PDO::PARAM_STR);
-            $this->_db->addParam(':controller', $controller,\PDO::PARAM_STR);
-            $this->_db->addParam(':action',     $action,    \PDO::PARAM_STR);
+            $this->db->clearParams();
+            $this->db->addParam(':level', $l, \PDO::PARAM_STR);
+            $this->db->addParam(':message', $message, \PDO::PARAM_STR);
+            $this->db->addParam(':controller', $controller, \PDO::PARAM_STR);
+            $this->db->addParam(':action', $action, \PDO::PARAM_STR);
 
-            $this->_db->doInsert();
-            $query = $this->_db->getResult();
+            $this->db->doInsert();
+            $query = $this->db->getResult();
 
             if ($query === false) {
-                $error = $this->_db->getMsgResult();
+                $error = $this->db->getMsgResult();
                 throw new \Exception('Log: '.var_export($error, true));
             }
 
             return true;
-        }
-        catch (\Exception $e) {
+        } catch (\Exception $e) {
             $this->_msgResult = $e->getMessage();
             return false;
         }
