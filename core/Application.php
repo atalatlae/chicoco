253702c4eab618772cb2ef6c7c7b2023c3a05ac4
Boris <atalatlae@gmail.com>
2017-11-26 00:22:30 -0300

    [apply_phpcs] improve the code style
    - remove _ (underscore) from attrbutes and method names
    - now use "} else {" for elses

--- core/Application.php
+++ core/Application.php
@@ -4,36 +4,36 @@ namespace Chicoco;
 
 class Application
 {
-    private static $_instance;
+    private static $instance;
 
-    private $_controller = '';
-    private $_action = '';
-    private $_uriParts = '';
-    private $_path ='';
-    private $_config;
-    private $_pathParams = array();
-    private $_alias = '';
+    private $controller = '';
+    private $action = '';
+    private $uriParts = '';
+    private $path ='';
+    private $config;
+    private $pathParams = array();
+    private $alias = '';
 
     public function __construct()
     {
         try {
             $this->loadConfig();
 
-            $this->_uriParts = parse_url($_SERVER['REQUEST_URI']);
-            $this->_path = explode('/', $this->_uriParts['path']);
+            $this->uriParts = parse_url($_SERVER['REQUEST_URI']);
+            $this->path = explode('/', $this->uriParts['path']);
 
-            if (isset($this->_path[1]) && $this->_path[1] != '') {
-                $this->_controller = $this->_path[1];
+            if (isset($this->path[1]) && $this->path[1] != '') {
+                $this->controller = $this->path[1];
             } else {
-                $this->_controller = 'Index';
+                $this->controller = 'Index';
             }
 
-            if (isset($this->_path[2]) && $this->_path[2] != '') {
-                $this->_action = $this->_path[2];
+            if (isset($this->path[2]) && $this->path[2] != '') {
+                $this->action = $this->path[2];
             } else {
-                $this->_action = 'Index';
+                $this->action = 'Index';
             }
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             header("HTTP/1.0 500 Error found");
             include('layout/500.phtml');
             exit();
@@ -42,37 +42,39 @@ class Application
 
     public static function getInstance()
     {
-        if (!self::$_instance instanceof self) {
-            self::$_instance = new self;
+        if (!self::$instance instanceof self) {
+            self::$instance = new self;
         }
-        return self::$_instance;
+        return self::$instance;
     }
 
-    private function _checkAlias() {
-        if (isset($this->_config['Aliases'])) {
-            $aliases = $this->_config['Aliases'];
-            $key = $this->_uriParts['path'];
+    private function checkAlias()
+    {
+        if (isset($this->config['Aliases'])) {
+            $aliases = $this->config['Aliases'];
+            $key = $this->uriParts['path'];
 
             foreach ($aliases as $k => $v) {
                 $patern = '$^'.$k.'$';
                 $r = preg_match($patern, $key);
                 if ($r === 1) {
-                    $this->_alias = $k;
-                    list($this->_controller, $this->_action) = explode("/", $v);
-                    $this->_addToGlobal('ALIAS', $k);
+                    $this->alias = $k;
+                    list($this->controller, $this->action) = explode("/", $v);
+                    $this->addToGlobal('ALIAS', $k);
                     break;
                 }
             }
         }
     }
 
-    private function _parcePathParams() {
-        $path = $this->_uriParts['path'];
+    private function parcePathParams()
+    {
+        $path = $this->uriParts['path'];
 
-        if ($this->_alias != '') {
-            $path = substr_replace($path, '', 0, strlen($this->_alias));
+        if ($this->alias != '') {
+            $path = substr_replace($path, '', 0, strlen($this->alias));
         } else {
-            $path = substr_replace($path, '', 0, strlen('/'.$this->_controller.'/'.$this->_action));
+            $path = substr_replace($path, '', 0, strlen('/'.$this->controller.'/'.$this->action));
         }
 
         $path = preg_replace('+^/+', '', $path);
@@ -93,37 +95,38 @@ class Application
                 }
             }
         }
-        $this->_pathParams = $pathParams;
+        $this->pathParams = $pathParams;
     }
 
-    public function run() {
+    public function run()
+    {
         try {
-            $this->_checkAlias();
+            $this->checkAlias();
 
-            $controller = $this->_controller.'Controller';
+            $controller = $this->controller.'Controller';
             $c = new $controller;
 
             if (!($c instanceof Controller)) {
-                throw new \Exception('Unable to load class '.$this->_controller);
+                throw new \Exception('Unable to load class '.$this->controller);
             }
 
-            $c->setController($this->_controller);
-            $c->setAction($this->_action);
+            $c->setController($this->controller);
+            $c->setAction($this->action);
 
-            $this->_parcePathParams();
+            $this->parcePathParams();
 
-            $c->setPathParams($this->_pathParams);
+            $c->setPathParams($this->pathParams);
             $c->init();
 
-            if (!method_exists($c, $this->_action.'Action')) {
-                throw new \Exception('Unable to execute the action "'.$this->_action.'"');
+            if (!method_exists($c, $this->action.'Action')) {
+                throw new \Exception('Unable to execute the action "'.$this->action.'"');
             }
 
-            $this->_addToGlobal('CONTROLLER', $this->_controller);
-            $this->_addToGlobal('ACTION', $this->_action);
-            $this->_addToGlobal('PATH_PARAMS', $this->_pathParams);
+            $this->addToGlobal('CONTROLLER', $this->controller);
+            $this->addToGlobal('ACTION', $this->action);
+            $this->addToGlobal('PATH_PARAMS', $this->pathParams);
 
-            $c->{$this->_action.'Action'}();
+            $c->{$this->action.'Action'}();
         } catch (\Exception $e) {
             header("HTTP/1.0 500 Error found");
             include('layout/500.phtml');
@@ -131,8 +134,9 @@ class Application
         }
     }
 
-    private function loadConfig() {
-        $this->_config = array();
+    private function loadConfig()
+    {
+        $this->config = array();
         if (!($config = @parse_ini_file('conf/Application.ini', true))) {
             throw new \Exception("unable to load configuration file");
         }
@@ -141,40 +145,42 @@ class Application
             // Get the general conf
             if (is_array($config['General'])) {
                 foreach ($config['General'] as $k => $v) {
-                    $this->_config[$k] = $v;
+                    $this->config[$k] = $v;
                 }
             }
 
             // Get the conf for the current env
-            if (isset($this->_config['applicaction.env']) && is_array($config[$this->_config['applicaction.env']])) {
-                foreach ($config[$this->_config['applicaction.env']] as $k => $v) {
-                    $this->_config[$k] = $v;
+            if (isset($this->config['applicaction.env']) && is_array($config[$this->config['applicaction.env']])) {
+                foreach ($config[$this->config['applicaction.env']] as $k => $v) {
+                    $this->config[$k] = $v;
                 }
             }
 
             // Get the common conf
             if (isset($config['Common']) && is_array($config['Common'])) {
                 foreach ($config['Common'] as $k => $v) {
-                    $this->_config[$k] = $v;
+                    $this->config[$k] = $v;
                 }
             }
 
             // Get the aliases conf
             if (isset($config['Aliases']) && is_array($config['Aliases'])) {
-                $this->_config['Aliases'] = array();
+                $this->config['Aliases'] = array();
                 foreach ($config['Aliases'] as $k => $v) {
-                    $this->_config['Aliases'][$k] = $v;
+                    $this->config['Aliases'][$k] = $v;
                 }
             }
-            $this->_addToGlobal('CONF', $this->_config);
+            $this->addToGlobal('CONF', $this->config);
         }
     }
 
-    public function getConfig() {
-        return $this->_config;
+    public function getConfig()
+    {
+        return $this->config;
     }
 
-    private function _addToGlobal($key, $value) {
+    private function addToGlobal($key, $value)
+    {
         global $_CHICOCO;
 
         if (!isset($_CHICOCO)) {
