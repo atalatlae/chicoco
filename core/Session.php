97d1ef2e5ab75b487b8bd189167edb442f157b2f
Boris <atalatlae@gmail.com>
2017-11-26 01:21:47 -0300

    [apply_phpcs] apply PSR2 into core/Session.php

--- core/Session.php
+++ core/Session.php
@@ -4,28 +4,33 @@ namespace Chicoco;
 
 class Session extends Singleton
 {
-    public function __construct() {
+    public function __construct()
+    {
         session_start();
     }
 
-    public function setVar($name, $value) {
+    public function setVar($name, $value)
+    {
         if ($name != "") {
             $_SESSION[$name] = $value;
         }
     }
 
-    public function getVar($name) {
-        if ($name != "" && isset($_SESSION[$name]) ) {
+    public function getVar($name)
+    {
+        if ($name != "" && isset($_SESSION[$name])) {
             return $_SESSION[$name];
         }
         return null;
     }
 
-    public function getSessionId() {
+    public function getSessionId()
+    {
         return session_id();
     }
 
-    public function destroy() {
+    public function destroy()
+    {
         unset($_SESSION);
         session_destroy();
     }
