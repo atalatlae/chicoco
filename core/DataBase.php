58e9ea7b2a00199a1c3745159fd7471d60f456d4
Boris <atalatlae@gmail.com>
2017-11-26 00:55:30 -0300

    [apply_phpcs] replace tabs by spaces

--- core/DataBase.php
+++ core/DataBase.php
@@ -4,41 +4,40 @@ namespace Chicoco;
 
 class DataBase extends \PDO
 {
-	protected static $_instance;
+    protected static $_instance;
+    protected $engine;
+    protected $host;
+    protected $port;
+    protected $dbname;
+    protected $username;
+    protected $pass;
 
-	protected $engine;
-	protected $host;
-	protected $port;
-	protected $dbname;
-	protected $username;
-	protected $pass;
+    public function __construct()
+    {
+        $app = Application::getInstance();
+        $conf = $app->getConfig();
 
-	public function __construct()
-	{
-		$app = Application::getInstance();
-		$conf = $app->getConfig();
+        $this->engine = $conf["database.engine"];
+        $this->host = $conf["database.host"];
+        $this->port = $conf["database.port"];
+        $this->dbname = $conf["database.dbname"];
+        $this->username = $conf["database.username"];
+        $this->pass = $conf["database.pass"];
 
-		$this->engine = $conf["database.engine"];
-		$this->host = $conf["database.host"];
-		$this->port = $conf["database.port"];
-		$this->dbname = $conf["database.dbname"];
-		$this->username = $conf["database.username"];
-		$this->pass = $conf["database.pass"];
+        try {
+            $dsn = $this->engine.":dbname=".$this->dbname."; host=".$this->host."; port=".$this->port;
+            parent::__construct($dsn, $this->username, $this->pass);
+        }
+        catch (\Exception $e) {
+            throw new \Exception('Database: error when try to connect: '.$e->getMessage());
+            return false;
+        }
+    }
 
-		try {
-			$dsn = $this->engine.":dbname=".$this->dbname."; host=".$this->host."; port=".$this->port;
-			parent::__construct($dsn, $this->username, $this->pass);
-		}
-		catch (\Exception $e) {
-			throw new \Exception('Database: error when try to connect: '.$e->getMessage());
-			return false;
-		}
-	}
-
-	public static function getInstance() {
-		if (  !self::$_instance instanceof self) {
-			self::$_instance = new self;
-		}
-		return self::$_instance;
-	}
+    public static function getInstance() {
+        if (  !self::$_instance instanceof self) {
+            self::$_instance = new self;
+        }
+        return self::$_instance;
+    }
 }
