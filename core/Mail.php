21dc35432a0d0ef8870ec81d701f4cbb73766c2a
Boris <atalatlae@gmail.com>
2017-11-26 01:17:34 -0300

    [apply_phpcs] apply PSR2 into core/Mail.php

--- core/Mail.php
+++ core/Mail.php
@@ -4,70 +4,71 @@ namespace Chicoco;
 
 class Mail
 {
-    private $_from;
-    private $_to;
-    private $_subject;
-    private $_content;
-    private $_data;
-    private $_layout;
+    private $from;
+    private $to;
+    private $subject;
+    private $content;
+    private $data;
+    private $layout;
+    private $message;
 
-    private $_message;
+    public function __construct($from, $to, $subject, $content, array $data, $layout = 'mail')
+    {
+        $this->from = $from;
+        $this->to = $to;
+        $this->subject = $subject;
+        $this->content = $content;
+        $this->data = $data;
+        $this->layout = $layout;
 
-    public function __construct($from = '', $to = '', $subject = '', $content = '', array $data, $layout = 'mail') {
-        $this->_from = $from;
-        $this->_to = $to;
-        $this->_subject = $subject;
-        $this->_content = $content;
-        $this->_data = $data;
-        $this->_layout = $layout;
-
-        $this->_render();
+        $this->render();
     }
 
-    public function sendHtml() {
-        $headers = "From: ".$this->_from."\r\n"
+    public function sendHtml()
+    {
+        $headers = "From: ".$this->from."\r\n"
         ."MIME-Version: 1.0\r\n"
         ."Content-Type: text/html; charset=utf-8\r\n";
 
-        return $this->_send($headers);
+        return $this->send($headers);
     }
 
-    private function _send($headers = '') {
-        return mail($this->_to, $this->_subject, $this->_message, $headers);
+    private function send($headers = '')
+    {
+        return mail($this->to, $this->subject, $this->message, $headers);
     }
 
-    private function _render() {
-        if ($this->_layout != '' && !is_file('layout/'.$this->_layout.'.phtml')) {
+    private function render()
+    {
+        if ($this->layout != '' && !is_file('layout/'.$this->layout.'.phtml')) {
             return false;
-        }
-        else if ($this->_layout == '') {
-            $this->_layout = 'mail';
+        } elseif ($this->layout == '') {
+            $this->layout = 'mail';
         }
 
-        if ($this->_content != '' && !is_file('view/'.$this->_content)) {
+        if ($this->content != '' && !is_file('view/'.$this->content)) {
             return false;
         }
 
         // Put the variables visible to the included file
-        if (is_array($this->_data)) {
-            foreach($this->_data as $k => $v) {
+        if (is_array($this->data)) {
+            foreach ($this->data as $k => $v) {
                 ${$k}  = $v;
             }
         }
 
-        if ($this->_content != '') {
+        if ($this->content != '') {
             ob_start();
-            include('view/'.$this->_content);
+            include('view/'.$this->content);
             $content = ob_get_contents();
             ob_end_clean();
-        }
-        else {
+        } else {
             $content = '';
         }
 
         ob_start();
-        include ('layout/'.$this->_layout.'.phtml');
-        $this->_message = ob_get_contents();
+        include('layout/'.$this->layout.'.phtml');
+        $this->message = ob_get_contents();
         ob_end_clean();
 
         return true;
